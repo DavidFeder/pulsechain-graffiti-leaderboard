e1be1fe02d7f60af6671d1785dda9ba3024b9927
David Feder <37136224+DavidFeder@users.noreply.github.com>
2026-05-28 22:07:25 -0400

    Fix external logo dependency - now uses locally bundled image

--- src/components/PulseChainLogo.tsx
+++ src/components/PulseChainLogo.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
 
+// IMPORTANT:
+// Copy LogoTransparent.png from your other repo into:
+// src/assets/LogoTransparent.png
+// Then commit the image to this repo.
+import logoSrc from '../assets/LogoTransparent.png';
+
 interface PulseChainLogoProps {
   className?: string;
   size?: number;
 }
 
 /**
- * PulseChain / ValidatorStore logo
- * Loaded from: https://github.com/DavidFeder/pulsechain-staking-launchpad-validatorstore
+ * PulseChain / ValidatorStore logo (bundled locally)
  */
 export function PulseChainLogo({ className = '', size = 40 }: PulseChainLogoProps) {
   return (
     <img
-      src="https://raw.githubusercontent.com/DavidFeder/pulsechain-staking-launchpad-validatorstore/main/LogoTransparent.png"
+      src={logoSrc}
       alt="PulseChain Logo"
       width={size}
       height={size}
